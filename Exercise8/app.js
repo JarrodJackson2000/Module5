7bc73f5e7cd30f79a8b5c56ae04f0d946819d74f
JarrodJackson2000 <jarrodjackson2000@hotmail.com>
2024-05-30 17:15:24 +1200

    added swagger

--- Exercise8/app.js
+++ Exercise8/app.js
@@ -8,4 +8,8 @@ const dataRouter = require("./routes/dataRoute");
 
 app.use("/Data", dataRouter);
 
+const swaggerUi = require("swagger-ui-express");
+swaggerDocument = require("./swagger.json");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 module.exports = app;
